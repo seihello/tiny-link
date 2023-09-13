f633df545800e03ddda76ce39d2db13582d52a98
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-13 16:53:26 -0700

    Receive POST login

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ authRouter.get("/login", (req, res) => {
   res.render("login"); // This will render the urls.ejs file
 });
 
+authRouter.post("/login", (req, res) => {
+  console.log("receive post login");
+});
+
 authRouter.get("/register", (req, res) => {
   res.render("register"); // This will render the urls.ejs file
 });

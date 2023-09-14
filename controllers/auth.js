ac2760f5a64fa3668f9b7c538925cbe6820e8220
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-09-14 11:31:06 -0700

    adding user to user.json

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,1 +1,27 @@
-// auth functions
+import { v4 as uuidv4 } from "uuid";
+import users from "../models/users.json" assert { type: "json" };
+import fs from 'fs';
+
+const newUser = async (req, res, next) => {
+  const receivedMail = req.body.email;
+  const receivedName = req.body.name;
+  const receivedPassword = req.body.password;
+  const id = uuidv4();
+
+  users[id] = {
+    id: id,
+    name: receivedName,
+    email: receivedMail,
+    password: receivedPassword,
+  };
+
+  const updatedUsers = { ...users };
+
+  fs.writeFileSync("models/users.json", JSON.stringify(updatedUsers, null, 2));
+
+  res.redirect("/");
+};
+
+export {newUser};
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 import express from "express";
+import {newUser} from"../controllers/auth.js";
+
 const authRouter = express.Router();
 
 authRouter.get("/login", (req, res) => {
@@ -8,5 +10,7 @@ authRouter.get("/login", (req, res) => {
 authRouter.get("/register", (req, res) => {
   res.render("register"); // This will render the urls.ejs file
 });
+
+authRouter.post("/register", newUser);
 
 export default authRouter;

bc9bec22a4a917d131daabd10059b71f8eaf12e1
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-14 13:35:46 -0700

    Return an error when the email is not registered

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,48 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+let users = {};
+
 // auth functions
 export const postLogin = (req, res) => {
   console.log(req.body.email);
   console.log(req.body.password);
-  if(userExists(req.body.email)) {
+
+  users = readUsers();
+
+  if(getUser(req.body.email)) {
     if(isPasswordValid(req.body.email, req.body.password)) {
       res.redirect("urls");
+    } else {
+      console.log("password incorrect");
     }
+  } else {
+    res.status(403).send("this email is not registered");
   }
 }
 
-function userExists(email) {
-  return true;
+function getUser(email) {
+  const user = Object.values(users).find(user => {
+    return user.email === email;
+  });
+
+  return user;
 }
 
 function isPasswordValid(email, password) {
   return true;
 }
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Function to read the URLs from the file
+const readUsers = () => {
+  const data = fs.readFileSync(
+    path.join(__dirname, "../models/users.json"), "utf8"
+  );
+
+  return JSON.parse(data);
+};

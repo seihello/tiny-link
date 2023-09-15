e6f9dd08ced8f1d5964f4c76c192c3eaa7b0dca7
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-09-15 15:10:56 -0700

    pw validations, encrypt pw

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,17 +5,23 @@ import { dirname } from "path";
 import { v4 as uuidv4 } from "uuid";
 import users from "../models/users.json" assert { type: "json" };
 
+import bcrypt from 'bcrypt';
+
+const saltRounds = 2; 
+
 const newUser = (req, res) => {
   const receivedMail = req.body.email;
   const receivedName = req.body.name;
   const receivedPassword = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  const id = uuidv4();
+
+  if (!receivedMail || !receivedPassword) {
+    return res.status(400).send("Email and password are required.");
+  }
 
   const checkUsers = readUsers();
 
   const users = Object.values(checkUsers);
-
   const checkEmail = users.some((user) => user.email === receivedMail);
 
   if (checkEmail) {
@@ -26,20 +32,36 @@ const newUser = (req, res) => {
     return res.status(400).send("Passwords do not match. Please try again.");
   }
 
-  const newUserObject = {
-    id: id,
-    name: receivedName,
-    email: receivedMail,
-    password: receivedPassword,
-  };
+  const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!password.test(receivedPassword)) {
+    return res.status(400).send("Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character.");
+  }
 
-  checkUsers[id] = newUserObject;
+  const id = uuidv4();
 
-  fs.writeFileSync("models/users.json", JSON.stringify(checkUsers, null, 2));
+  bcrypt.hash(receivedPassword, saltRounds, (err, hash) => {
+    if (err) {
+      return res.status(500).send("Error encrypting password.");
+    }
 
-  res.redirect("/");
+    const newUserObject = {
+      id: id,
+      name: receivedName,
+      email: receivedMail,
+      password: hash, 
+    };
+
+    checkUsers[id] = newUserObject;
+
+    fs.writeFileSync("models/users.json", JSON.stringify(checkUsers, null, 2));
+
+    req.session.login = true; 
+
+    res.redirect("/urls");
+  });
 };
-export {newUser};
+
+export { newUser };
 
 // auth functions
 export const postLogin = (req, res) => {
@@ -67,7 +89,7 @@ function getUser(email, users) {
 }
 
 function isPasswordValid(user, password) {
-  return user.password === password;
+  return bcrypt.compareSync(password, user.password);
 }
 
 export function postLogout(req, res) {

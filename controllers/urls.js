19be7ed129d84618c3c87d994251c5c4e59ad5b4
EduBox <123496427+EduardoEduBox@users.noreply.github.com>
2023-09-19 13:20:23 -0300

    now its true, it is DONE!

--- controllers/urls.js
+++ controllers/urls.js
@@ -28,4 +28,14 @@ const writeUrls = (urls) => {
   );
 };
 
-export { readUrls, writeUrls };
+function generateShortUrl() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+export { readUrls, writeUrls, generateShortUrl };
